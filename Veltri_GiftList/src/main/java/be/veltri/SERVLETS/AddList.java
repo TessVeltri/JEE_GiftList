99e708796ec60ad33d5c46c4fec5c5c2906c823b
Veltri <tess.veltri@condorcet.be>
2022-12-29 17:06:00 +0100

    Finish btn modify and delete in addList

--- Veltri_GiftList/src/main/java/be/veltri/SERVLETS/AddList.java
+++ Veltri_GiftList/src/main/java/be/veltri/SERVLETS/AddList.java
@@ -7,6 +7,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import be.veltri.JAVABEANS.Gift;
+import be.veltri.JAVABEANS.GiftList;
+
 /**
  * Servlet implementation class AddList
  */
@@ -24,6 +27,15 @@ public class AddList extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String idGift = request.getParameter("idGift");
+		if (idGift != null) {
+			int index = Integer.parseInt(idGift);
+			GiftList gl = (GiftList) request.getSession().getAttribute("addList");
+			Gift gift = gl.getLstGift().get(index);
+			gl.deleteLstGift(gift);
+			request.getSession().setAttribute("addList", gl);
+		}
+		
 		request.getRequestDispatcher("/WEB-INF/JSP/AddList.jsp").forward(request,response);
 	}
 
---
+++ Veltri_GiftList/src/main/java/be/veltri/SERVLETS/ModifyGift.java
@@ -0,0 +1,56 @@
+package be.veltri.SERVLETS;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import be.veltri.JAVABEANS.Gift;
+import be.veltri.JAVABEANS.GiftList;
+
+/**
+ * Servlet implementation class ModifyGift
+ */
+public class ModifyGift extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ModifyGift() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String where = request.getParameter("where");
+		String idGift = request.getParameter("idGift");
+		int index = Integer.parseInt(idGift);
+		if (where.equals("local")) {
+			GiftList gl = (GiftList) request.getSession().getAttribute("addList");
+			Gift gift = gl.getLstGift().get(index);
+			
+			request.setAttribute("modifyGift", gift);
+			request.setAttribute("where", where);
+			request.setAttribute("idGift", idGift);
+		} else {
+			// vient de la DB
+		}
+		
+		
+		
+		request.getRequestDispatcher("/WEB-INF/JSP/ModifyGift.jsp").forward(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
